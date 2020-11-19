1c59071ccb3fe84fe0e3c883e0d929dce04584dc
Jingzhou7 <55573333+Jingzhou7@users.noreply.github.com>
2020-11-19 13:53:43 -0500

    dispatcher and worker functions implemented corner case: from lowercase to uppercase

--- dispatcher/dispatcher.java
+++ dispatcher/dispatcher.java
@@ -1,3 +1,10 @@
+/**
+ * @Author: Jingzhou Xue
+ */
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 public class Dispatcher {
 
     /**
@@ -11,9 +18,50 @@ public class Dispatcher {
      *      "ttttt" ~ "zzzzz"
      * 返回的数据结构自选
      */
-    public String[][] getJobList(int numOfWorkers) {
+    public ArrayList<Job> getJobList(int numOfWorkers) {
+        ArrayList<Job> jobList = new ArrayList<Job>();
+        int total = (int) Math.pow(26, 5);
+        int workload = total / numOfWorkers;
+        String start = "aaaaa";
+        for (int i = 0; i < numOfWorkers - 1; i++) {
+            Job p = getJobFrom(start, workload);
+            jobList.add(p);
+            start = increment(start, workload);
+        }
+        jobList.add(new Job(start, "zzzzz"));
+        return jobList;
+    }
+    private Job getJobFrom(String start, int workload) {
+        String end = increment(start, workload);
+        return new Job(start, end);
+    }
 
-        return null;
+    public String increment(String s, int step) {
+        // TODO: wrap around zzzzz to AAAAA
+        // check remaining distance from s to zzzzz and update step if step is larger
+        // start from AAAAA and use new step
+        char baseChar = 'a';
+        if (Character.isUpperCase(s.charAt(0))) {
+            baseChar = 'A';
+        }
+        char[] chars = new char[5];
+        if (s.length() != 5) return "";
+        int position = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = 5 - 1 - i;
+            position += Math.pow(BASE, i) * (s.charAt(charIdx) - baseChar);
+        }
+        position += step;
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = 5 - 1 - i;
+            // System.out.println("number: " + position % BASE);
+            chars[charIdx] = (char) (baseChar + position % BASE);
+            position /= BASE;
+            // System.out.println(chars);
+            // System.out.println("position: " + position);
+
+        }
+        return String.valueOf(chars);
     }
 
     public static void main(String[] args) {
--- worker/worker.java
+++ worker/worker.java
@@ -1,6 +1,9 @@
 
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
+import java.security.NoSuchAlgorithmException;
 /**
- * @Author: 
+ * @Author: Jingzhou Xue
  */
 public class Worker {
 
@@ -16,13 +19,63 @@ public class Worker {
      * @param{String} md5: the md5 of the correct password.
      * @return{String} the correct password or null.
      */
-    public String bruteForcingPassword(String start, String end, String md5) {
+    private final int BASE = 26;
 
+    public String bruteForcingPassword(String start, String end, String md5) {
+        try {
+            while(!start.equals(end)) {
+                if (encrypt(start).equals(md5)) {
+                    return start;
+                }
+                start = increment(start, 1);
+            }
+        } catch (NoSuchAlgorithmException nsae) {
+
+        }
+        return "";
+    }
+    // TODO: function increment is the same in dispatcher and worker. get rid of one.
+    public String increment(String s, int step) {
+        // TODO: wrap around zzzzz to AAAAA
+        // check remaining distance from s to zzzzz and update step if step is larger
+        // start from AAAAA and use new step
+        char baseChar = 'a';
+        if (Character.isUpperCase(s.charAt(0))) {
+            baseChar = 'A';
+        }
+        char[] chars = new char[5];
+        if (s.length() != 5) return "";
+        int position = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = 5 - 1 - i;
+            position += Math.pow(BASE, i) * (s.charAt(charIdx) - baseChar);
+        }
+        position += step;
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = 5 - 1 - i;
+            // System.out.println("number: " + position % BASE);
+            chars[charIdx] = (char) (baseChar + position % BASE);
+            position /= BASE;
+            // System.out.println(chars);
+            // System.out.println("position: " + position);
+
+        }
+        return String.valueOf(chars);
+    }
+
+    private String encrypt(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest);
+        return myHash;
     }
 
     public static void main(String[] args) {
         Worker worker = new Worker();
-        String password = worker.bruteForcingPassword();
+        // TODO: get start, end, and md5
+        String start, end, md5 = "";
+        String password = worker.bruteForcingPassword(start, end, md5);
         System.out.println(password);
     }
 
