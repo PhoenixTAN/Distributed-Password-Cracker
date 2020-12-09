98d9a7ec8843fb310bbe191e502d5590fe44b9b5
Cayden <cayden_ykj@163.com>
2020-12-09 17:52:02 -0500

    add screen recording and update simulation high concurrency file by Kaijia You

--- simulationHighConcurrency.py
+++ simulationHighConcurrency.py
@@ -1,25 +1,37 @@
 import gevent
 from gevent import monkey
+# 补丁
 monkey.patch_all()
 
 import time
 import json
 from urllib.error import URLError
 import requests
+import hashlib
+import string
+import random
 
 
 
 
-
-url = 'http://192.12.245.163:8080/getPassword'
+url = 'http://192.86.139.65:8080/getPassword'
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 
 
 def make_data():
     """the body of post request"""
+    charlist = [random.choice(string.ascii_letters) for i in range(5)]
+    while(not ((charlist[0] >= 'a' and charlist[0] <= 'c') and (charlist[1] >= 'a' and charlist[1] <= 'c'))):
+        charlist = [random.choice(string.ascii_letters) for i in range(5)]
+    chars = ''.join(charlist)
+    print(chars)
+    # create md5 object
+    hl = hashlib.md5()
+    hl.update(chars.encode(encoding='utf-8'))
+    print(hl.hexdigest())
     data = {
-        'MD5Password': '594f803b380a41396ed63dca39503542',
-        'workerNum': '5'
+        'MD5Password': hl.hexdigest(),
+        'workerNum': '8'
     }
     return json.dumps(data)
 
@@ -27,7 +39,6 @@ def make_data():
 def run():
     data = make_data()
     try:
-        # s3:requests请求
         resp = requests.post(url=url, data=data, headers=headers)
         print("password:\n", resp.content.decode())
 
